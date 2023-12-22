d5c17154f9b8aee200e5e0c0a890f546661c7fa7
oddfar <oddfar@163.com>
2023-12-22 12:11:01 +0800

    refactor: campus框架升级到V1.1.8

--- campus-common/src/main/java/com/oddfar/campus/common/domain/entity/SysUserRoleEntity.java
+++ campus-common/src/main/java/com/oddfar/campus/common/domain/entity/SysUserRoleEntity.java
@@ -5,17 +5,20 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * 用户和角色关联 sys_user_role
  */
 @Data
 @TableName("sys_user_role")
-public class SysUserRoleEntity {
+public class SysUserRoleEntity implements Serializable {
     private static final long serialVersionUID = 1L;
     /**
      * 用户ID
      */
 //    @TableId(type = IdType.AUTO)
+    @TableId(type = IdType.INPUT)
     private Long userId;
 
     /**
